9abe7b1f5829259c45b50464571dd8f8129e92f5
madhav <madhav@gmail.com>
2021-01-08 15:33:56 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+var users={
+name:"Alex",
+age:20,
+company:"Qspiders",
+skills:["manaual Testng","api testing","java"],
+//data:function(){
+//console.log(this.name;
+//},
+}
+var  JSONdata = JSON.stringify(users);
+console.log(JSONData);
